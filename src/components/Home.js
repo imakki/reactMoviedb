41b99ca18360cdb8f82a390bbc449bb434edda7a
Ankit <ankityc143>
2020-01-09 14:15:44 +0530

    styled heroImage component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import {
     API_KEY,
     API_BASE_URL,
     POSTER_SIZE,
-    BACKDROP_SIZE
+    BACKDROP_SIZE,
+    IMAGE_BASE_URL
 } from '../config';
 
 //import components
@@ -23,9 +24,16 @@ const Home = () => {
     const [{ state, loading, error }, fetchMovies] = useHomeFetch();
     console.log(state);
 
+    if(error) return <div>Something went wrong.....</div>;
+    if(!state.movies[0]) return <Spinner/>;
+
     return (
         <>
-            <HeroImage />
+            <HeroImage
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+                title={state.heroImage.original_title}
+                text={state.heroImage.overview}
+             />
             <SearchBar />
             <Grid />
             <MovieThumb />
