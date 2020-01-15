2629f6cc785f609b2b3027baa3a8f7c52cc72f16
Ankit <ankityc143>
2020-01-15 13:35:33 +0530

    hook added for movie fetch of movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,18 +8,27 @@ import Actor from './elements/Actor';
 import Grid from './elements/Grid';
 import Spinner from './elements/Spinner';
 
-const Movie = ({ movieId }) => (
+import { useMovieFetch } from './hooks/useMovieFetch';
 
-    <>
-        <Navigation />
-        <MovieInfo />
-        <MovieInfoBar />
-        <Grid>
-            <Actor />
-        </Grid>
-        <Spinner />
-    </>
+const Movie = ({ movieId }) => {
 
-);
+    const [movie, loading, error] = useMovieFetch(movieId);
+    console.log(movie);
+
+    return (
+
+        <>
+            <Navigation />
+            <MovieInfo />
+            <MovieInfoBar />
+            <Grid>
+                <Actor />
+            </Grid>
+            <Spinner />
+        </>
+
+    )
+
+};
 
 export default Movie;
---
+++ src/components/hooks/useMovieFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect, useCallback } from 'react';
+import { API_URL, API_KEY } from '../../config';
+
+export const useMovieFetch = movieId => {
+    const [state, setState] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchData = useCallback(async () => {
+
+        setError(false);
+        setLoading(true);
+
+        try {
+            const endPoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+            const result = await (await fetch(endPoint)).json();
+            const creditEndPoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+            const creditsResult = await (await fetch(creditEndPoint)).json();
+    
+            const directors = creditsResult.crew.filter(
+                member => member.job === 'Director'
+            );
+
+            setState({
+                ...result,
+                actors: creditsResult.cast,
+                directors,
+            })
+
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+
+    }, [movieId])
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData])
+
+    return [state, loading, error];
+} 
