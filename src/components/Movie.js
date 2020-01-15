4a60c465174579c2ddae38b38ccfb892bdef985d
Ankit <ankityc143>
2020-01-15 14:24:20 +0530

    styled movie info component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -15,16 +15,18 @@ const Movie = ({ movieId }) => {
     const [movie, loading, error] = useMovieFetch(movieId);
     console.log(movie);
 
+    if(error) return <div>Something went wrong.....</div>;
+    if(loading) return <Spinner />
+
     return (
 
         <>
-            <Navigation />
-            <MovieInfo />
+            <Navigation movie={movie.original_title} />
+            <MovieInfo movie={movie} />
             <MovieInfoBar />
             <Grid>
                 <Actor />
             </Grid>
-            <Spinner />
         </>
 
     )
